bd14eaf89b2c187a8a973280018c779aae850acc
the-jossie <ayanfe.dev@gmail.com>
2024-04-03 19:33:05 +0100

    feat: ProductReview model

---
+++ ShopDotCo-API/Models/ProductReview.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopDotCo_API.Models
+{
+    public class ProductReview
+    {
+        public required string Id { get; set; }
+        public required string Name { get; set; }
+        public required string? ProductId { get; set; }
+        public required double Rating { get; set; }
+        public required string Comment { get; set; }
+    }
+}
