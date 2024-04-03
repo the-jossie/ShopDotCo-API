22339acca6ebfa4dc78fa215c8439dccb4a90b4b
the-jossie <ayanfe.dev@gmail.com>
2024-04-03 22:12:37 +0100

    fix: decimal errors

--- ShopDotCo-API/Data/DataContext.cs
+++ ShopDotCo-API/Data/DataContext.cs
@@ -26,6 +26,12 @@ namespace ShopDotCo_API.Data
                 .WithMany(p => p.Reviews)
                 .HasForeignKey(r => r.ProductId);
 
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(t => t.GetProperties())
+                .Where(p => p.ClrType == typeof(decimal) || p.ClrType == typeof(decimal?)))
+            {
+                property.SetColumnType("decimal(18,2)");
+            }
         }
     }
 }
